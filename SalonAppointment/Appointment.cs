349422bc030c302f95679f38bce6ce980ba18d72
anjana-varadan <104071363+anjana-varadan@users.noreply.github.com>
2022-10-27 11:13:12 -0400

    Appointment module

---
+++ SalonAppointment/Appointment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalonAppointment
+{
+    internal class Appointment : IComparable<Appointment>
+    {
+        public int Time { get; set; }
+        public string Name { get; set; }
+
+        public string Service { get; set; }
+        public double price { get; set; }
+        public double Total { get; set; }
+
+
+        //function to display appointment and select service based on patient type
+        public void print()
+        {
+
+            Console.Write($"{Name.ToUpper()} have appointment at {Time / Program.minutes_in_hour:00}:{Time % Program.minutes_in_hour:00} and {Service} {Total}\n");
+
+        }
+        public int CompareTo(Appointment other)
+        {
+            return this.Time - other.Time;
+        }
+    }
+}
