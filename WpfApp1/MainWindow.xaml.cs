cac457b73f6bd0a4d1ea81589bdd3ae78dfbe81e
anjana-varadan <104071363+anjana-varadan@users.noreply.github.com>
2022-10-27 11:03:50 -0400

    WPF module

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            tb.Text += b.Content.ToString();
+        }
+
+
+        private void Button_Result(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                result();
+            }
+            catch (Exception)
+            {
+                tb.Text = "Error!";
+            }
+        }
+
+        private void result()
+        {
+            String op;
+            int iOp = 0;
+            if (tb.Text.Contains("+"))
+            {
+                iOp = tb.Text.IndexOf("+");
+            }
+            else if (tb.Text.Contains("-"))
+            {
+                iOp = tb.Text.IndexOf("-");
+            }
+            else if (tb.Text.Contains("*"))
+            {
+                iOp = tb.Text.IndexOf("*");
+            }
+            else if (tb.Text.Contains("/"))
+            {
+                iOp = tb.Text.IndexOf("/");
+            }
+            else
+            {
+                //error    
+            }
+
+            op = tb.Text.Substring(iOp, 1);
+            double op1 = Convert.ToDouble(tb.Text.Substring(0, iOp));
+            double op2 = Convert.ToDouble(tb.Text.Substring(iOp + 1, tb.Text.Length - iOp - 1));
+
+            if (op == "+")
+            {
+                tb.Text =  (op1 + op2).ToString();
+            }
+            else if (op == "-")
+            {
+                tb.Text += "=" + (op1 - op2);
+            }
+            else if (op == "*")
+            {
+                tb.Text += "=" + (op1 * op2);
+            }
+            else
+            {
+                tb.Text += "=" + (op1 / op2);
+            }
+        }
+        private void Button_Clear(object sender, RoutedEventArgs e)
+        {
+            tb.Text = "";
+        }
+    }
+}
