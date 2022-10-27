91c6d15a3614d9560775e71ac23dad8c1d4b60d1
anjana-varadan <104071363+anjana-varadan@users.noreply.github.com>
2022-10-27 11:11:41 -0400

    Baggage info module

---
+++ BaggageInfo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BaggageInfo
+{
+    internal class Program
+    {
+        enum BagTypes
+        {
+            Bag1,
+            Bag2,
+            ComputerBag,
+            HandBag
+        }
+        static void Main(string[] args)
+        {
+            var BagTypes = Enum.GetValues(typeof(BagTypes));
+            var totalWeight = 0m;
+            Console.WriteLine("Where did you flew from?");
+            var origin = Console.ReadLine();
+            foreach (var bag in BagTypes)
+            {
+                Console.WriteLine($"Did you have {bag} luggage option?(Y/N)");
+                if (Console.ReadLine().ToLower().Contains("y"))
+                {
+                    Console.WriteLine("How much do they weigh (kg)?");
+                    if (decimal.TryParse(Console.ReadLine(), out var weight))
+                    {
+                        totalWeight += weight;
+                    }
+                }
+
+            }
+            Console.WriteLine($"I flew from {origin} with {totalWeight:n} kg of luggage");
+            Console.ReadLine();
+        }
+    }
+}
